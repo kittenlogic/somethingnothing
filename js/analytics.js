0c57ac974a49a38409b36b74ef764f3854b1229f
Emily Martinez <mle626@gmail.com>
2024-11-29 00:26:47 -0800

    updated GA4 tracking code

--- js/analytics.js
+++ js/analytics.js
@@ -1,12 +1,16 @@
-//Google Analytics somethingnothing.me
+// Google Analytics GA4 Tag
+(function() {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4B1BBS8EWC';
+  document.head.appendChild(script);
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'G-4B1BBS8EWC']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
+  script.onload = function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-4B1BBS8EWC');
+  };
+})();
